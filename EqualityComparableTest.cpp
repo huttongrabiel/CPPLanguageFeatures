b60c583a81dec65e6f475d6dc983dd10d77e0eda
huttongrabiel <huttonthomas@icloud.com>
2022-06-04 10:13:17 -0700

    Concepts: First look at C++ 20 concepts
    Interesting note is that the example of concepts in Bjarne's Tour of
    C++ is before these were implemented and were experimental so the ->
    bool syntax is no longer used in the requires clause. Instead
    std::convertible_to or std::same_as are used.

---
+++ EqualityComparableTest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <concepts>
+#include <stdlib.h>
+
+template<typename T>
+concept Equality_Comparable = 
+    requires(T a, T b) {
+        { a==b } -> std::convertible_to<bool>;
+        { a!=b } -> std::convertible_to<bool>;
+    };
+
+template<Equality_Comparable T>
+void print_t(T value)
+{
+    std::cout << value << std::endl;
+}
+
+struct a {
+    int z;
+};
+
+int main() {
+
+    static_assert(Equality_Comparable<a>);
+
+    std::cout << "Asserted" << std::endl;
+}
